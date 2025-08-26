b6601ba965d0c4da81c3cc6278da1cd243ddef8a
edgaldo <edgaldo@gmail.com>
2025-08-26 18:23:03 -0500

    sitio inicial

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("navbar.html")
+    .then(res => res.text())
+    .then(html => document.getElementById("navbar").innerHTML = html);
+
+  fetch("footer.html")
+    .then(res => res.text())
+    .then(html => document.getElementById("footer").innerHTML = html);
+});
